e0b5aab332f93f0f56256faed4a8b48053e1ce59
jivimiguel <jgpucio@up.edu.ph>
2026-02-19 18:06:05 +0800

    Reflect on spatial overlay and classification process

--- server/analysis.py
+++ server/analysis.py
@@ -51,4 +51,30 @@ overlay["percentage"] = (
 
 overlay["percentage"] = overlay["percentage"].round(2)
 
+# print(overlay.head())
+
+# get dominant land use for per parcel
+idx = overlay.groupby("parcel_pin")["percentage"].idxmax()
+
+dominant = overlay.loc[idx, ["parcel_pin", "name", "percentage"]]
+
+# dissolve (aggregate) by parcel_pin to get percentage and dominant land use
+geom = overlay[["parcel_pin", "geometry"]].dissolve(
+    by="parcel_pin").reset_index()
+
+# merge dominant land use back to dissolved geometries
+overlay = geom.merge(
+    dominant,
+    on="parcel_pin",
+    how="left"
+)
+
 print(overlay.head())
+
+dominant_res = overlay[
+    ((overlay["name"] == "Residential Zone - Low Density") |
+    (overlay["name"] == "Residential Zone - Medium Density")) &
+    (overlay["percentage"] >= 60)
+].copy()
+
+print(dominant_res.head())
